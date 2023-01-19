ab62786909cf4995290899c0483005e2fb3de8b7
katherinepanora <121049195+katherinepanora@users.noreply.github.com>
2023-01-19 12:50:22 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -12,12 +12,24 @@ $(".pbutton3").click(function() {
 
 $(".project1,.project2,.project3").hide();
 $(".allProjs").click(function() {
+    $(".project1open,.project2open,.project3open").hide();
     $(".aboutMe,.contactInfo").hide();
+    $(".contactMe").hide();
     $(".project1,.project2,.project3").show();
 });
 
 $(".aboutMe,.contactInfo ").hide();
 $(".personalInfo").click(function() {
     $(".project1,.project2,.project3").hide();
+    $(".project1open,.project2open,.project3open").hide();
+    $(".contactMe").hide();
     $(".aboutMe,.contactInfo ").show();
+});
+
+$(".contactMe").hide();
+$(".contacts").click(function() {
+    $(".project1,.project2,.project3").hide();
+    $(".project1open,.project2open,.project3open").hide();
+    $(".aboutMe,.contactInfo ").hide();
+    $(".contactMe").show();
 });
