d147380f743b5c0c6e5a47be509be8f94318d12d
katherinepanora <121049195+katherinepanora@users.noreply.github.com>
2023-05-05 12:02:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(".project1open,.project2open,.project3open").hide();
+$(".project1open,.project2open,.project3open,.project4open").hide();
 
 $(".pbutton1").click(function() {
     $(".project1open").toggle();
@@ -9,27 +9,30 @@ $(".pbutton2").click(function() {
 $(".pbutton3").click(function() {
     $(".project3open").toggle();
 });
+$(".pbutton4").click(function() {
+    $(".project4open").toggle();
+});
 
-$(".project1,.project2,.project3").hide();
+$(".project1,.project2,.project3,.project4").hide();
 $(".allProjs").click(function() {
     $(".project1open,.project2open,.project3open").hide();
     $(".aboutMe,.contactInfo").hide();
     $(".contactMe").hide();
-    $(".project1,.project2,.project3").show();
+    $(".project1,.project2,.project3,.project4").show();
 });
 
 $(".aboutMe,.contactInfo ").hide();
 $(".personalInfo").click(function() {
-    $(".project1,.project2,.project3").hide();
-    $(".project1open,.project2open,.project3open").hide();
+    $(".project1,.project2,.project3,.project4").hide();
+    $(".project1open,.project2open,.project3open,.project4open").hide();
     $(".contactMe").hide();
     $(".aboutMe,.contactInfo ").show();
 });
 
 $(".contactMe").hide();
 $(".contacts").click(function() {
-    $(".project1,.project2,.project3").hide();
-    $(".project1open,.project2open,.project3open").hide();
+    $(".project1,.project2,.project3,.project4").hide();
+    $(".project1open,.project2open,.project3open,.project4open").hide();
     $(".aboutMe,.contactInfo ").hide();
     $(".contactMe").show();
 });
